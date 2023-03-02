fe7ae7b3dd341c70ee4ee305d43b90d03a53e08f
shubender <2andrey.verbitskiy@gmail.com>
2023-03-02 12:39:19 +0100

    add "googleusercontent" to whitelist

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['links.papareact.com']
+    domains: ['links.papareact.com', 'googleusercontent.com']
   },
   experimental: {
     appDir: true
