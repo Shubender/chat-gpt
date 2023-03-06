384e0a197337c96cdd7c5ebcc7257c77617df7cb
shubender <2andrey.verbitskiy@gmail.com>
2023-03-06 21:36:49 +0100

    add chat row

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,11 +1,21 @@
 'use client'
 
+import { collection, orderBy, query } from "firebase/firestore";
 import { useSession, signOut } from "next-auth/react"
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
+import ChatRow from "./ChatRow";
 import NewChat from "./NewChat"
 
 function SideBar() {
   const { data: session } = useSession();
 
+  const [chats, loading, error] = useCollection(
+    session && query(collection(db, "users", session?.user?.email!, "chats"), orderBy('createdAt', 'desc'))
+  );
+
+  console.log(chats);
+    
   return (
     <div className="p-2 flex flex-col h-screen">
         <div className="flex-1">
@@ -17,6 +27,9 @@ function SideBar() {
                 </div>
 
                 {/* Map through the ChatRows */}
+                {chats?.docs.map(chat => (
+                  <ChatRow key={chat.id} id={chat.id}/>
+                ))}
             </div>
         </div>
         {session && <img onClick={() => signOut()} src={session.user?.image!} alt="user-image" className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50" />}
