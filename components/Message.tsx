49f3b92b98089089b24a49c68bc7b1a4f26cd0c5
shubender <2andrey.verbitskiy@gmail.com>
2023-03-14 15:40:54 +0100

    crete Message body

--- components/Message.tsx
+++ components/Message.tsx
@@ -5,7 +5,16 @@ type Props = {
 };
 
 function Message({ message }: Props) {
-  return <div>Message</div>;
+  const isChatGPT = message.user.name === "ChatGPT";
+
+  return (
+    <div className={`py-5 text-white ${isChatGPT && "bg-[#434654]"}`}>
+      <div className="flex space-x-5 px-10 max-w-2xl mx-auto">
+        <img src={message.user.avatar} className="h-8 w-8" />
+        <p className="pt-1 text-sm">{message.text}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Message;
