55f392ea08caa7f477b84b9918c8d68f3d11ce87
shubender <2andrey.verbitskiy@gmail.com>
2023-03-14 14:03:11 +0100

    add massage query and answer

--- pages/api/askQuestion.ts
+++ pages/api/askQuestion.ts
@@ -1,6 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import query from "../../lib/queryApi";
+import admin from "firebase-admin";
+import { adminDb } from "../../firebaseAdmin";
+// import { measureMemory } from "vm";
 
 type Data = {
   answer: string;
@@ -26,5 +29,23 @@ export default async function handler(
 
   const response = await query(prompt, chatId, model);
 
-  // res.status(200).json({ name: "John Doe" });
+  const message: Message = {
+    text: response || "ChatGPT was unable to find and answer for that!",
+    createdAt: admin.firestore.Timestamp.now(),
+    user: {
+      _id: "ChatGPT",
+      name: "ChatGPT",
+      avatar: "https://links.papareact.com/89k",
+    },
+  };
+
+  await adminDb
+    .collection("users")
+    .doc(session?.user?.email)
+    .collection("chats")
+    .doc(chatId)
+    .collection("messages")
+    .add(message);
+
+  res.status(200).json({ answer: message.text });
 }
