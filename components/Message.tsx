bf84bde6d4833ec0bb2c8926e3f4ab7047d65574
shubender <2andrey.verbitskiy@gmail.com>
2023-03-14 14:23:58 +0100

    create Message component

---
+++ components/Message.tsx
@@ -0,0 +1,11 @@
+import { DocumentData } from "firebase/firestore";
+
+type Props = {
+  message: DocumentData;
+};
+
+function Message({ message }: Props) {
+  return <div>Message</div>;
+}
+
+export default Message;
